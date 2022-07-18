5bf6b85f44f2beb50a6d29e008ebb904dcf7962f
GabSouza <gabrielaraujodesouza98@gmail.com>
2022-07-17 22:27:37 -0300

    Altera padrao da mensagem

--- backend/src/main/java/com/devsuperior/dsmeta/service/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SmsService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
 import java.util.Optional;
 
 @Service
@@ -35,8 +36,9 @@ public class SmsService {
 
         String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 
-        String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(), date, sale.getAmount());
-
+        String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+                + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
+        
         Twilio.init(twilioSid, twilioKey);
 
         PhoneNumber to = new PhoneNumber(twilioPhoneTo);
